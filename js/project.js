f7d948d7521aff8f78d18c0ba577fa7391dfc9a5
Abdyrakhman Taskibayev <taskibaev.ab@gmail.com>
2024-05-16 15:07:48 +0500

    register functions finished aproximitly

--- js/project.js
+++ js/project.js
@@ -1,5 +1,6 @@
 var projectClass = function() {
-    var api_url = "https://script.google.com/macros/s/AKfycbw1wAThtRLXq5606G7nbhJk_bpFdF7eHfGDNOL4LpxFdwsmsw5mxykXQBR21ZTD7E00/exec";
+    // var api_url = "https://script.google.com/macros/s/AKfycbw1wAThtRLXq5606G7nbhJk_bpFdF7eHfGDNOL4LpxFdwsmsw5mxykXQBR21ZTD7E00/exec";
+    var api_url = "https://script.google.com/macros/s/AKfycbySmaITEMroIEzoE6wwKEW9mchZx9tZ8e3ZLnse0HmL8Hf7ydE5-SwS1s2F5gI2Dca2/exec";
     var default_service_id = "myaccount";
 
     var tempStorage = new tempDataStorage;
